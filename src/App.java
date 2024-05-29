05b3abf2d176aaa6e7279ea19853d7d3f2fa73c7
LissethVasquez04 <vasquezlisseth004@gmail.com>
2024-05-29 11:53:19 -0600

    ejercicio 1 finalizado

--- src/App.java
+++ src/App.java
@@ -1,5 +1,47 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Iterator;
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+   
+        ArrayList<Productos> Producto = new ArrayList<>();
+
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("Ingrese el ID del producto:");
+            int id = scanner.nextInt();
+            scanner.nextLine();
+            System.out.println("Ingrese el nombre del producto:");
+            String nombre = scanner.nextLine();
+            System.out.println("Ingrese la marca del producto:");
+            String marca = scanner.nextLine();
+            System.out.println("Ingrese el precio del producto:");
+            float precio = scanner.nextFloat();
+            scanner.nextLine();
+            Productos productos = new Productos(id, nombre, marca, precio);
+            Producto.add(productos);
+
+            System.out.println("¿Desea agregar otro producto? (Continuar agregando/no, dejar de ingresar)");
+            String respuesta = scanner.nextLine();
+            if (respuesta.equalsIgnoreCase("no, dejar de ingresar")) {
+                break; // Sale del bucle
+            }
+           
+
+    }
+    Collections.sort(Producto, Comparator.comparingInt(Productos::getId));
+        Iterator<Productos> iterator = Producto.iterator();
+        while (iterator.hasNext()) {
+            Productos productoActual = iterator.next();
+            System.out.println("id: " + productoActual.getId() +
+                    ", Nombre: " + productoActual.getNombre() +
+                    ", Marca: " + productoActual.getMarca() +
+                    ", Precio: " + productoActual.getPrecio());
+        }
+        scanner.close();
     }
 }
---
+++ src/Productos.java
@@ -0,0 +1,49 @@
+public class Productos {
+
+    private int id;
+    private String nombre;
+    private String marca;
+    private float precio;
+    public Productos() {
+    }
+    public Productos(int id, String nombre, String marca, float precio) {
+        this.id = id;
+        this.nombre = nombre;
+        this.marca = marca;
+        this.precio = precio;
+    }
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    public String getMarca() {
+        return marca;
+    }
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+    public float getPrecio() {
+        return precio;
+    }
+    public void setPrecio(float precio) {
+        this.precio = precio;
+    }
+
+    @Override
+    public String toString() {
+        return "Producto{" +
+                "id=" + id +
+                ", nombre='" + nombre + '\'' +
+                ", marca='" + marca + '\'' +
+                ", precio=" + precio +
+                '}';
+    }   
+}
